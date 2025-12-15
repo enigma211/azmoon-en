54e3a2f99f60ae970e3f4256eea1ddfc8dd24c42
enigma211 <enigma2.king@gmail.com>
2025-12-16 00:20:19 +0330

    flashcard3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -195,9 +195,9 @@
                     </li>
                     <li>
                         <a href="{{ route('flashcards.index') }}" wire:navigate class="flex flex-col items-center gap-1 py-2 md:py-3 text-gray-600 hover:text-indigo-600">
-                            <!-- Heroicon: rectangle-stack -->
+                            <!-- Heroicon: academic-cap -->
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 md:h-6 md:w-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 8.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v8.25A2.25 2.25 0 006 14.25h2.25S9 14.25 9 20.25m7.5-12v8.25M20.25 12v8.25" />
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.499 5.216 50.59 50.59 0 00-2.658.812m-15.482 0a50.57 50.57 0 012.658.812m15.482 0a50.57 50.57 0 002.658-.812" />
                             </svg>
                             <span>Flashcards</span>
                         </a>
